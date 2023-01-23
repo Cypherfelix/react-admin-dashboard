227e14917098967c55f9928ec4605316e8024612
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-23 14:05:30 +0300

    Creation of Table

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Cards from "../Cards/Cards";
+import Table from "../Table/Table";
 import "./MainDash.css";
 const MainDash = () => {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
       <Cards />
-      <div className="table"></div>
+      <Table />
     </div>
   );
 };
